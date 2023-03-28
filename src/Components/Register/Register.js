9e0d1e36a71dfcfa9275dba6459ca887fa63d757
dmitr <85069792+letoproject@users.noreply.github.com>
2023-03-28 19:34:18 +0300

    Update Register.js

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
 	}
 
 	onSubmitSignIn = () => {
-		fetch('https://still-waters-90987.herokuapp.com/register', {
+		fetch('https://prismatic-sorbet-a97828.netlify.app/register', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
